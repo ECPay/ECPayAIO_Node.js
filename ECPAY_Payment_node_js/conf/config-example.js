b1ded340ef82d372fb4e481a40a8e1b7b9149e5f
allen.chang <allennchang@gmail.com>
2024-12-03 16:05:22 +0800

    調整測試廠商編號及KEY/IV

--- ECPAY_Payment_node_js/conf/config-example.js
+++ ECPAY_Payment_node_js/conf/config-example.js
@@ -1,9 +1,9 @@
 const options = {
   "OperationMode": "Test", //Test or Production
   "MercProfile": {
-    "MerchantID": "2000132",
-    "HashKey": "5294y06JbISpM5x9",
-    "HashIV": "v77hoKGq4kWxNNIS"
+    "MerchantID": "3002607",
+    "HashKey": "pwFHCqoQZGmho4w6",
+    "HashIV": "EkRm7iFT261dpevs"
   },
   "IgnorePayment": [
 //    "Credit",
